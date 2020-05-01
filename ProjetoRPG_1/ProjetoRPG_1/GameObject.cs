bd19f646d4f474fb4d0980a1722b15753f0208b3
be3013 <57907227+be3013@users.noreply.github.com>
2020-05-01 19:35:05 -0300

    Correção

--- ProjetoRPG_1/ProjetoRPG_1/GameObject.cs
+++ ProjetoRPG_1/ProjetoRPG_1/GameObject.cs
@@ -1,10 +1,12 @@
-﻿using System;
+using System;
 
 namespace ProjetoRPG
 {
     public class GameObject
     {
         public Personagem P { get; set; }
+
+        public Monstro M { get; set; }
         public Item I { get; set; }
         public bool O { get; set; }
         public int Linha { get; set; }
