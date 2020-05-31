de7fe62062e38bb9318d44d4c555fa94c6966c1a
Patrick Guilherme <patrick.guilhermedasilva@gmail.com>
2020-05-31 15:29:04 -0300

    Update CasosTeste.cs
    Todos os 9 casos de teste principais
    
    Co-Authored-By: jotalima72 <jotalima72@users.noreply.github.com>
    Co-Authored-By: danielbb21 <danielbb21@users.noreply.github.com>
    Co-Authored-By: be3013 <be3013@users.noreply.github.com>

--- ProjetoRPG_1/TesteRPG/CasosTeste.cs
+++ ProjetoRPG_1/TesteRPG/CasosTeste.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using ProjetoRPG;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TesteRPG
 {
@@ -39,15 +40,166 @@ namespace TesteRPG
             Assert.IsTrue(wk.EquiparEquipamento(ep));
         }
         [Test]
-        public void PodeMover_PosicaoIndisponivel() 
+        public void AtaqueMenorIgual_Defesa()
         {
-            Worker wk = new Worker();
-            wk.PosicaoX = 0;
-            wk.PosicaoY = 0;
-            Mapa GeradorMapa = new Mapa();
-            GeradorMapa.ConstruirMapa();
-            Assert.IsFalse(wk.PodeMover(GeradorMapa.MapaJogo, wk.PosicaoY, wk.PosicaoY));
+            Worker worker = new Worker
+            {
+                Nome = "João None",
+                Descricao = "",
+                Life = 300,
+                Energia = 500,
+                MenuCraft = new Craft(),
+                MaxEnergia = 500,
+                MaxLife = 300,
+                Animo = 20,
+                Nivel = 1,
+                Conhecimento = 0,
+                Persistencia = 15,
+                PosicaoX = 19,
+                PosicaoY = 2,
+                inventario = new Inventario()
+                {
+                    Itens = new List<Item>()
+                },
+                Habilidades = new List<Habilidade>()
+
+            };
+            worker.StartHabilidade();
+
+            Gasefic gasefic = new Gasefic
+            {
+                Animo = 15,
+                ConhecimentoDrop = 5,
+                Life = 400,
+                Energia = 150,
+                Persistencia = 30
+            };
+            //defesa == ataque
+            int result = worker.atacar(gasefic, null, worker.Habilidades.ElementAt<Habilidade>(0));
+            Assert.AreEqual(0, result);
+
+            //defesa maior ataque
+            gasefic.Persistencia = 40;
+            result = worker.atacar(gasefic, null, worker.Habilidades.ElementAt<Habilidade>(0));
+            Assert.AreEqual(0, result);
+
         }
+        [Test]
+        public void AtaqueMaior_Defesa()
+        {
+            Worker worker = new Worker
+            {
+                Nome = "João None",
+                Descricao = "",
+                Life = 300,
+                Energia = 500,
+                MenuCraft = new Craft(),
+                MaxEnergia = 500,
+                MaxLife = 300,
+                Animo = 20,
+                Nivel = 1,
+                Conhecimento = 0,
+                Persistencia = 15,
+                PosicaoX = 19,
+                PosicaoY = 2,
+                inventario = new Inventario()
+                {
+                    Itens = new List<Item>()
+                },
+                Habilidades = new List<Habilidade>()
+
+            };
+            worker.StartHabilidade();
+
+            Gasefic gasefic = new Gasefic
+            {
+                Animo = 15,
+                ConhecimentoDrop = 5,
+                Life = 400,
+                Energia = 150,
+                Persistencia = 10
+            };
+
+            /*life: 200
+             animo: 20
+             dano da habilide: 10;
+             dano habilidade + animo: 30
+
+             persistencia: 10
+             */
+            int result = worker.atacar(gasefic, null, worker.Habilidades.ElementAt<Habilidade>(0));
+            Assert.AreEqual(20, result);
+        }
+        [Test]
+        public void HabilidadeNoListNivelMaiorIgual_nivelRequerido()
+        {
+            Worker worker = new Worker();
+            worker.Nivel = 1;
+            worker.Conhecimento = 240;
+            worker.Habilidades = new List<Habilidade>();
+            worker.LevelUp();
+
+            Assert.AreEqual("Força do Ódio", worker.Habilidades.Last().Nome);
+
+            worker.Conhecimento = 408;
+            worker.LevelUp();
+
+            Assert.AreEqual("Força do Ódio",worker.Habilidades.ElementAt<Habilidade>(0).Nome);
+        }
+        [Test]
+        public void HabilidadeNoListNivelMenor_nivelRequerido()
+        {
+            Worker worker = new Worker();
+            worker.Nivel = 1;
+            worker.Habilidades = new List<Habilidade>();
+            worker.StartHabilidade();
+
+            Assert.AreNotEqual("Força do Ódio",worker.Habilidades.Last().Nome);
+        }
+        [Test]
+        public void ConhecimentoMaiorIgual_Requerido()
+        {
+            Worker worker = new Worker();
+            worker.Nivel = 1;
+            worker.Conhecimento = 240;//igual
+            worker.Habilidades = new List<Habilidade>();
+            worker.LevelUp();
+
+            Assert.AreEqual(2, worker.Nivel);//igual
+
+            worker.Conhecimento = 500;
+            worker.LevelUp();
+
+            Assert.AreEqual(3,worker.Nivel);//maior
+        }
+        [Test]
+        public void ConhecimentoMenor_Requerido()
+        {
+            Worker worker = new Worker();
+            worker.Nivel = 1;
+            worker.Conhecimento = 239;
+            worker.Habilidades = new List<Habilidade>();
+            worker.LevelUp();
+
+            Assert.AreEqual(1, worker.Nivel);//igual
+        }
+        [Test]
+        public void ConhecimentoRestante_posLevelUp()
+        {
+            Worker worker = new Worker();
+            worker.Nivel = 1;
+            worker.Conhecimento = 240;
+            worker.Habilidades = new List<Habilidade>();
+            worker.LevelUp();
+
+            Assert.AreEqual(0,worker.Conhecimento);//zerar conhecimento
+
+            worker.Conhecimento = 420;
+            worker.LevelUp();
+
+            Assert.AreEqual(12, worker.Conhecimento);//restante do nivel anterior
+        }
+    
     }
 }
 
