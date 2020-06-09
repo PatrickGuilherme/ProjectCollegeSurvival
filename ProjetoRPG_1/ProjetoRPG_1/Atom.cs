57b70d0f2811dd19bb514a72488e3d7a18c7a965
Danielbb21 <50447725+Danielbb21@users.noreply.github.com>
2020-06-09 11:27:23 -0300

    Implementação dos métodos

--- ProjetoRPG_1/ProjetoRPG_1/Atom.cs
+++ ProjetoRPG_1/ProjetoRPG_1/Atom.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace ProjetoRPG
@@ -33,15 +33,71 @@ namespace ProjetoRPG
             ip.Nome = "Mecanismo Eletrônico";
             this.ItemDrop = ip;
         }
+        public bool fumaca() {
+            Habilidade habilidade = new Habilidade();
+            habilidade.Nome = "fumaca";
+            habilidade.Descricao = " utiliza folhas de papel afiadas e atira elas no inimigo.";
+            habilidade.NivelRequerido = 1;
+            habilidade.GastoEnergia = 0;
+            habilidade.Dano = 10;
+            habilidade.BuffLife = 0;
+            habilidade.BuffAnimo = 0;
+            habilidade.BuffPersistencia = 0;
+            habilidade.DesativaHabilidade = false;
+            habilidade.Usada = false;
+            habilidade.Ativa = true;
+            this.Habilidades.Add(habilidade);
+            return true;
+        }
+
+        public bool ArremessoQuimico() {
+            Habilidade habilidade = new Habilidade();
+            habilidade.Nome = "Arremesso químico";
+            habilidade.Descricao = " Ataque utilizando vidrarias e lançando elas contra o inimigo.";
+            habilidade.NivelRequerido = 1;
+            habilidade.GastoEnergia = 50;
+            habilidade.Dano = 20;
+            habilidade.BuffLife = 0;
+            habilidade.BuffAnimo = 0;
+            habilidade.BuffPersistencia = 0;
+            habilidade.DesativaHabilidade = false;
+            habilidade.Usada = false;
+            habilidade.Ativa = true;
+            this.Habilidades.Add(habilidade);
+            return true;
+        }
+
+        public bool PoDaMorte() {
+            Habilidade habilidade = new Habilidade();
+            habilidade.Nome = "Pó da Morte";
+            habilidade.Descricao = " Ataque de grande dano ao inimigo.";
+            habilidade.NivelRequerido = 1;
+            habilidade.GastoEnergia = 70;
+            habilidade.Dano = 25;
+            habilidade.BuffLife = 0;
+            habilidade.BuffAnimo = 0;
+            habilidade.BuffPersistencia = 0;
+            habilidade.DesativaHabilidade = false;
+            habilidade.Usada = false;
+            habilidade.Ativa = true;
+            this.Habilidades.Add(habilidade);
+            return true;
+        }
         public override bool StartHabilidade()
         {
-            throw new NotImplementedException();
-            throw new NotImplementedException();
+            fumaca();
+            ArremessoQuimico();
+            PoDaMorte();
+            return true;
         }
 
         public override bool UsarHabilidade(Habilidade habilidade)
         {
-            throw new NotImplementedException();
+            this.Animo += habilidade.BuffAnimo;
+            this.Life += habilidade.BuffLife;
+            this.Persistencia += habilidade.BuffPersistencia;
+            habilidade.Usada = true;
+            return true;
         }
     }
 }
